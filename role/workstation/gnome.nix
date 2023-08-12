63e41edf4f57c8ae59d023881d9145c4658eb89e
Philipp Millar <philipp.millar@poxar.net>
2023-08-12 17:44:25 +0200

    Add some small gnome utilities

--- role/workstation/gnome.nix
+++ role/workstation/gnome.nix
@@ -14,13 +14,19 @@
     amberol # music player
     emote # emoji picker
     espanso-wayland # global text expansion
+    eyedropper # color picker
     gnome-solanum # pomodoro timer
     gnome.gnome-tweaks # some additional gnome settings
     gnomeExtensions.espresso # keep the screen alive
     gnomeExtensions.run-or-raise # mighty shortcut manager
     gnomeExtensions.tiling-assistant # manual tiling
+    khronos # time tracker
+    metadata-cleaner # clean metadata from media files
     pika-backup # borg backup frontend
     pinentry-gnome # only here explictily for rbw
+    textpieces # simple text transformations
+    video-trimmer # simpler than raw ffmpeg sometimes
+    warp # graphical wormhole implementation
   ];
 
   environment.gnome.excludePackages = with pkgs; [
