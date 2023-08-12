6a5a9c9158f43fd32262d27893f7a4e5685e7edd
Philipp Millar <philipp.millar@poxar.net>
2023-08-12 14:34:54 +0200

    Include espanso in gnome

--- role/workstation/gnome.nix
+++ role/workstation/gnome.nix
@@ -7,8 +7,12 @@
   services.xserver.displayManager.gdm.wayland = true;
   services.xserver.desktopManager.gnome.enable = true;
 
+  # needed for espanso
+  users.users.pmi.extraGroups = [ "input" ];
+
   environment.systemPackages = with pkgs; [
     emote
+    espanso-wayland
     gnome-solanum
     gnome.gnome-tweaks
     gnomeExtensions.espresso
