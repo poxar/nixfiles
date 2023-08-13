384b4e37a870f09a2e3ab7bfb6fec8a74c4b5160
Philipp Millar <philipp.millar@poxar.net>
2023-08-13 20:34:08 +0200

    Automatically clean up garbage

--- common/nix.nix
+++ common/nix.nix
@@ -1,6 +1,15 @@
 {
+  nix.settings.auto-optimise-store = true;
+
+  # keep shell.nix derivations
   nix.extraOptions = ''
     keep-outputs = true
     keep-derivations = true
   '';
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
 }
