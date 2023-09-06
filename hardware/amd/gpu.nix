8450c993ea0329be7d7619c2791a48b4146108b9
Philipp Millar <philipp.millar@poxar.net>
2023-09-06 17:06:08 +0200

    Allow raising power limits on AMD GPUs

--- hardware/amd/gpu.nix
+++ hardware/amd/gpu.nix
@@ -3,6 +3,7 @@
 {
   boot.initrd.kernelModules = [ "amdgpu" ];
   # boot.kernelParams = [ "radeon.si_support=0" "amdgpu.si_support=1" ];
+  boot.kernelParams = [ "amdgpu.ppfeaturemask=0xffffffff" ];
   services.xserver.videoDrivers = [ "amdgpu" ];
 
   hardware.opengl.extraPackages = with pkgs; [
