8d23d5291ee6821f14e38ca57d2e661381f34033
Philipp Millar <philipp.millar@poxar.net>
2023-09-06 17:06:32 +0200

    Set font features globally

--- role/workstation/fonts.nix
+++ role/workstation/fonts.nix
@@ -3,14 +3,36 @@
 {
   nixpkgs.config.joypixels.acceptLicense = true;
 
-  fonts.enableDefaultFonts = true;
-  fonts.fonts = with pkgs; [
-    (nerdfonts.override { fonts = [ "FiraCode" ]; } )
-    fira-code
-    inconsolata
-    inter
-    joypixels
-    noto-fonts
-    noto-fonts-cjk
-  ];
+  fonts = {
+    enableDefaultFonts = true;
+
+    fonts = with pkgs; [
+      (nerdfonts.override { fonts = [ "FiraCode" ]; } )
+      fira-code
+      inconsolata
+      inter
+      joypixels
+      noto-fonts
+      noto-fonts-cjk
+    ];
+
+    fontconfig.localConf = ''
+      <fontconfig>
+        <match target="font">
+          <test name="family" compare="eq" ignore-blanks="true">
+            <string>FiraCode</string>
+          </test>
+          <edit name="fontfeatures" mode="append">
+            <string>cv06 on</string>
+            <string>ss02 on</string>
+            <string>ss03 on</string>
+            <string>ss04 on</string>
+            <string>ss05 on</string>
+            <string>ss08 on</string>
+            <string>onum on</string>
+          </edit>
+        </match>
+      </fontconfig>
+    '';
+  };
 }
