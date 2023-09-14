3514990cd75cf47721fbcda5e582944e490ea4e3
Philipp Millar <philipp.millar@poxar.net>
2023-09-14 18:19:51 +0200

    Remove xterm

--- role/workstation/gnome.nix
+++ role/workstation/gnome.nix
@@ -3,6 +3,7 @@
 {
   services.xserver.enable = true;
   services.xserver.autorun = true;
+  services.xserver.excludePackages = [ pkgs.xterm ];
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.displayManager.gdm.wayland = true;
   services.xserver.desktopManager.gnome.enable = true;
