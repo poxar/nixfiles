663d900ad58cb6703fe6a219e529a13dd58309ea
Philipp Millar <philipp.millar@poxar.net>
2024-02-28 23:40:07 +0100

    Add support for unstable packages

--- flake.nix
+++ flake.nix
@@ -3,12 +3,23 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }@attrs: {
+  outputs = { self, nixpkgs, nixpkgs-unstable }:
+  let
+    system = "x86_64-linux";
+    overlay-unstable = final: prev: {
+      unstable = import nixpkgs-unstable {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    };
+  in {
     nixosConfigurations.leonis = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+      inherit system;
       modules = [
+        ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-unstable ]; })
         ./host/leonis
         ./hardware/cpu-amd.nix
         ./hardware/gpu-amd.nix
@@ -18,8 +29,9 @@
       ];
     };
     nixosConfigurations.tauron = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+      inherit system;
       modules = [
+        ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-unstable ]; })
         ./host/tauron
         ./hardware/cpu-intel.nix
         ./hardware/gpu-intel.nix
