5e93322af0166514dc83e0d35a3021dfa7fee76e
Philipp Millar <philipp.millar@poxar.net>
2024-02-28 23:54:03 +0100

    More keyd remaps

--- host/tauron/default.nix
+++ host/tauron/default.nix
@@ -18,6 +18,10 @@
       main = {
         capslock = "overload(control, esc)";
 	space = "overload(symbols, space)";
+
+        control = "oneshot(control)";
+        leftalt = "oneshot(alt)";
+        rightalt = "oneshot(altgr)";
       };
       "symbols" = {
         h = "left";
@@ -32,6 +36,9 @@
 	e = "end";
 
         s = "macro(M-right 10ms A-tab 10ms M-left 10ms A-tab)";
+
+        "[" = "macro(C-pageup)";
+        "]" = "macro(C-pagedown)";
       };
     };
   };
