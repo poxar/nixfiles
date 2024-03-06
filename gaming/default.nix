215856550ae5e3b09d5e416870287a3e0643645f
Philipp Millar <philipp.millar@poxar.net>
2024-03-06 18:54:31 +0100

    Remove ratbag/piper

--- gaming/default.nix
+++ gaming/default.nix
@@ -4,11 +4,7 @@
   programs.steam.enable = true;
   programs.gamemode.enable = true;
 
-  # mouse configuration
-  services.ratbagd.enable = true;
-
   environment.systemPackages = with pkgs; [
     mangohud
-    piper # mouse configuration
   ];
 }
